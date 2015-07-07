1f991f933530d94c0cdd316797a1cc7096caf6c9
Gigoo25 <robster105@gmail.com>
2015-07-06 20:56:03 -0400

    Update system_prop.mk
    Camera fixes

--- system_prop.mk
+++ system_prop.mk
@@ -18,6 +18,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qc.sdk.camera.facialproc=false \
+    ro.qc.sdk.gestures.camera=false \
     camera2.portability.force_api=1
 
 # Display
